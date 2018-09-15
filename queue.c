076fc34a3d4b0f9cbd4b9127afdd4a84b5348e36
pramodnitmz <pramodnitmz@gmail.com>
2018-09-16 01:35:49 +0530

    stack queue and linklist

---
+++ queue.c
@@ -0,0 +1,89 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct node{
+	int data;
+	struct node *next;
+}node;
+
+node *head=NULL, *rear;
+
+void insert();
+void delete();
+void display();
+
+int main()
+{
+	int c;
+	node *temp;
+
+	while(1)
+	{
+		printf("1.Insert\n2.Delete\n3.Exit\nEnter your choice : ");
+		scanf("%d",&c);
+		switch(c)
+		{
+			case 1:
+				insert();
+				break;
+
+			case 2:
+				delete();
+				break;
+
+			case 3:
+				exit(0);
+
+			default:
+				printf("Invalid Choice!\n");
+		}
+	}
+
+	return 0;
+}
+
+void insert()
+{
+	int value;
+	printf("Enter the value: ");
+	scanf("%d", &value);
+
+	node *temp = malloc(sizeof(node));
+	temp->data = value;
+	temp->next = NULL;
+
+	if (head == NULL)
+	{
+		head = temp;
+		rear = temp;
+	}
+	else
+	{
+		rear->next = temp;
+		rear = temp;
+	}
+	display();
+}
+
+void delete()
+{
+	node *temp=head;
+	if(head != NULL)	head = head->next;
+	free(temp);
+	display();
+}
+
+void display()
+{
+	node *temp = head;
+	if(head == NULL)
+	{
+		printf("List is Empty!\n");
+		return;
+	}
+	while(temp != NULL)
+	{
+		printf("%d ", temp->data);
+		temp = temp->next;
+	}
+}
---
+++ stack.c
@@ -0,0 +1,93 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct node{
+	int data;
+	struct node *next;
+}node;
+
+node *head=NULL, *rear;
+
+void insert();
+void delete();
+void display();
+
+int main()
+{
+	int c;
+	node *temp;
+
+	while(1)
+	{
+		printf("1.Insert\n2.Delete\n3.Clear Screen\n4.Exit\nEnter your choice : ");
+		scanf("%d",&c);
+		switch(c)
+		{
+			case 1:
+				insert();
+				break;
+
+			case 2:
+				delete();
+				break;
+
+			case 3:
+				system("tput clear");
+				break;
+
+			case 4:
+				exit(0);
+
+			default:
+				printf("Invalid Choice!\n");
+		}
+	}
+
+	return 0;
+}
+
+void insert()
+{
+	int value;
+	printf("Enter the value: ");
+	scanf("%d", &value);
+
+	node *temp = malloc(sizeof(node));
+	temp->data = value;
+	temp->next = NULL;
+
+	if (head == NULL)
+	{
+		head = temp;
+		rear = temp;
+	}
+	else
+	{
+		temp->next = head;
+		head = temp;
+	}
+	display();
+}
+
+void delete()
+{
+	node *temp=head;
+	if(head != NULL)	head = head->next;
+	free(temp);
+	display();
+}
+
+void display()
+{
+	node *temp = head;
+	if(head == NULL)
+	{
+		printf("List is Empty!\n");
+		return;
+	}
+	while(temp != NULL)
+	{
+		printf("%d\n", temp->data);
+		temp = temp->next;
+	}
+}
